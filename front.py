0a507cfc202a60aaf45895fc2232472a0f97a35a
ShubhraBajpai <for.shubhra@gmail.com>
2025-04-17 23:02:53 +0530

    imported model to front.py

--- front.py
+++ front.py
@@ -3,8 +3,7 @@ from PIL import Image
 import numpy as np
 import tensorflow as tf
 
-# Load your MobileNetV2 model
-#model = tf.keras.models.load_model('mobilenetv2_food_classifier.h5')
+model = tf.keras.models.load_model('food_detection_model.h5')
 
 # Subcategories and mapping to categories
 subcategories = [
@@ -59,7 +58,7 @@ st.write("Upload an image of a food item and we'll tell you what it is!")
 uploaded_file = st.file_uploader("Upload an image", type=["jpg", "jpeg", "png"])
 
 if uploaded_file is not None:
-    image = Image.open(uploaded_file)
+    image = Image.open(uploaded_file).convert('RGB')
     st.image(image, caption="Uploaded Image", use_column_width=True)
 
     # Preprocess for MobileNetV2
